c0bb2a5f38900372e763caf7c4e1c09730283cdd
Illni <illneralexandra@gmail.com>
2016-11-14 16:46:29 +0100

    palindrome ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,21 @@
-def palindrome(str):
-    return
+def palindrome(strng):
+    strng_without = strng.replace(" ","").lower()
+    revstr =  strng_without[::-1]
+    if revstr == strng_without :
+        return True
+    else:
+        return False
 
 
 def main():
-    return
+   string = input('Enter a string : ')
+   print(palindrome(string))
+
+
+    
 
 
 if __name__ == '__main__':
     main()
+
+
