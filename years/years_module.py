f933ec22155d96dcfcdcd2fce414784e2555e6cb
Jakubinyi <jakubinyi@gmail.com>
2016-11-14 21:08:39 +0100

    years ready

--- years/years_module.py
+++ years/years_module.py
@@ -2,12 +2,15 @@ import datetime
 
 
 def years(age):
-    return
+    date = datetime.datetime.now()
+    turn_date = date.year + 100 - age
+    return turn_date
 
 
 def main():
-    return
-
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    print(str(name) + "'s will turn 100 years old in : " + str(years(age)))
 
 if __name__ == '__main__':
     main()
