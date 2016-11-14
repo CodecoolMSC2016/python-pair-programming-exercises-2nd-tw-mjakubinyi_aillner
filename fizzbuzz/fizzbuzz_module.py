62f77e2a3296b7e9e09a9025f695450f59f8b593
Illni <illneralexandra@gmail.com>
2016-11-14 14:48:24 +0100

    fizzbuzz ready

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,21 @@
 def fizzbuzz(number):
-    return
+    if number%3 == 0 and number%5 == 0:
+        return('FizzBuzz')
+    elif number%3 == 0 :
+        return('Fizz')
+    elif number%5 == 0 :
+        return('Buzz')
+    elif number%3 != 0 and number%5 != 0:
+        return(number)
+
 
 
 def main():
-    return
+    num_list = []
+    for num in range(1,101):
+        num_list.append(fizzbuzz(num))
+        print(num_list[num-1])
 
+print("__name__=" + __name__)
 if __name__ == '__main__':
     main()
